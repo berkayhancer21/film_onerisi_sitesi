e3c3b2625a8591600d282e888ae082206dadad6e
Berkay Hançer <berkayhancer21@gmail.com>
2024-12-23 21:20:00 +0300

    push

---
+++ film_onerisi/app/page.tsx
@@ -0,0 +1,11 @@
+import AcmeLogo from '@/app/ui/acme-logo';
+import { ArrowRightIcon } from '@heroicons/react/24/outline';
+import Link from 'next/link';
+
+export default function Page() {
+  return (
+      <div className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black">
+          SELAMMM
+      </div>
+  );
+}
